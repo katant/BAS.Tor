e09be1b12476cbf3acf916b850f66251cf51f75c
Katant Savelev <katant.savelev@yandex.ru>
2020-01-25 03:00:07 +0300

    Change Tor IP (1.2)

---
+++ Tor_TorNewIp_select.js
@@ -0,0 +1,12 @@
+var Port = GetInputConstructorValue("Port", loader);
+if(Port["original"].length == 0)
+{
+  Invalid("Port is empty");
+  return;
+}
+try{
+          var code = loader.GetAdditionalData() + _.template($("#Tor_TorNewIp_code").html())({port:Port["original"]});
+          code = Normalize(code,0);
+          BrowserAutomationStudio_Append("", BrowserAutomationStudio_SaveControls() + code, action, DisableIfAdd);
+        }catch(e)
+        {}
