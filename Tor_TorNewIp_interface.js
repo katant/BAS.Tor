24a3da4fbc75302dc7a7c1c44e21dbc182e33d01
Лапки <lapocki@tuta.io>
2020-01-25 03:00:07 +0300

    Change Tor IP (1.2)

---
+++ Tor_TorNewIp_interface.js
@@ -0,0 +1,7 @@
+<div class="container-fluid">
+  <%= _.template($('#input_constructor').html())({id:"Port", description:tr("Port"), default_selector: "int"}) %>
+
+</div>
+<div class="tooltipinternal tr" style="margin-bottom:5px">Make new Tor circuit by port</div>
+
+<%= _.template($('#back').html())({action:"executeandadd", visible:true}) %>
