cd41c80b8ebdc9da2e886529e49d175648140690
Лапки <lapocki@tuta.io>
2020-11-02 21:17:56 +0300

    Fix variable issue (probably) and moved back to netcat for killing Tor (1.7)

--- Tor_TorNewIp_select.js
+++ Tor_TorNewIp_select.js
@@ -1,11 +1,11 @@
 var Port = GetInputConstructorValue("Port", loader);
-if (Port["original"].length == 0) {
+if (Port["updated"].length == 0) {
     Invalid("Port is empty");
     return;
 }
 try {
     var code = loader.GetAdditionalData() + _.template($("#Tor_TorNewIp_code").html())({
-        port: Port["original"]
+        port: Port["updated"]
     });
     code = Normalize(code, 0);
     BrowserAutomationStudio_Append("", BrowserAutomationStudio_SaveControls() + code, action, DisableIfAdd);
