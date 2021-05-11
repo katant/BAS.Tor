7774d23c2555405bb6f65c0e3afc18b7b4285258
Лапки <lapocki@tuta.io>
2021-05-11 14:19:26 +0300

    Tor binary update, netcat -> ncat (no more antivirus false positives) (1.9)

--- Tor_KillTor_code.js
+++ Tor_KillTor_code.js
@@ -13,7 +13,11 @@ PROCESS_STANDART_OUTPUT = _result()
 }
 sleep(1000)!
 native("filesystem", "removefile", RANDOM_FILE)
+try{
 PID = PROCESS_STANDART_OUTPUT.split('pid=')[1].split(' ')[0]
+}catch(e){
+fail("Tor module error: Can't find Tor PID by port " + TOR_PORT + ". Probably your Tor instance isn't running.")
+}
 RANDOM_FILE = "temp_" + rand() + ".bat"
 native("filesystem", "writefile", JSON.stringify({path: RANDOM_FILE,value: "chcp 65001\n" + (("".length>0) ? ("cd " + "" + "\n") : "" ) + ("taskkill /F /PID " + PID),base64:false,append:false}))
 native_async("processmanager", "start", JSON.stringify({location: RANDOM_FILE, working_folder: "", waitfinish: true, arguments: "", version: 2}))!
